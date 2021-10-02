98c20d98e4f5b8fab9d679637c42295f29893cd0
Itay Perl <itay.perl@gmail.com>
2021-10-02 23:54:36 +0300

    get rid of nodejs code in emscripten output

--- build_js.sh
+++ build_js.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 emcc mecab/src/.libs/libmecab.so -o lib/libmecab.js -g \
-    -s MODULARIZE -s EXPORT_ES6=1 \
+    -s MODULARIZE -s EXPORT_ES6=1 -s ENVIRONMENT="web,webview,worker" \
     -s ASSERTIONS=1 -s SAFE_HEAP=1 -s STACK_OVERFLOW_CHECK=1 -s INITIAL_MEMORY=199950336 \
     -s EXPORTED_FUNCTIONS='["_mecab_new2", "_mecab_sparse_tostr3"]' \
     -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap","UTF8ToString","lengthBytesUTF8"]' \
